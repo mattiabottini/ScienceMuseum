398dcad057cc0995acd1c5352e1975a13d5d94d6
Mattia Bottini <mattia@MBP-di-Mattia.station>
2022-03-18 17:43:22 +0100

    Css reservation

--- index.php
+++ index.php
@@ -5,20 +5,19 @@
         <script src="HomePageJS.js"></script>
     </head>
     <body>
-            <header id="header"> <br><a class="header" href="./HomePage.html">National Museum of Colorado</a></header>
+            <header id="header"> <br><a class="header" href="./index.php">National Museum of Colorado</a></header>
             <article id="contentHome"> <h2 id="changeText">Botanical Museum</h2>
                 <a class= "opening" href="#openingtimes">Info</a>
                 <a class= "contact" href="./book.html">Book a Guided visit</a>
             </article>
             <div class="navbar">
-                <a href="./HomePage.html">Home</a>
+                <a href="./index.php">Home</a>
                 <div class="subnav">
                   <button class="subnavbtn">Collections <i class="fa fa-caret-down"></i></button>
                   <div class="subnav-content">
                     <a href="./botany.html">Botany</a>
                     <a href="./zoology.html">Zoology</a>
                     <a href="./mineralogy.html">Mineralogy</a>
-                    <a href="./archeology.html">Archeology</a>
                   </div>
                 </div>
                 <div class="subnav">
@@ -28,7 +27,7 @@
                     <a href="./book.html">Book a visit</a>
                   </div>
                 </div>
-                <a href="#contact">Contact</a>
+                <a href="./contact.html">Contact</a>
               </div>
             <footer id="footer"> &copy Copyright 2022 - Mattia Bottini </footer>
     </body>
